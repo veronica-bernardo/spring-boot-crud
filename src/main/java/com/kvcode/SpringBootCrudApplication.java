fb14f13190eee8090602ff1eff0d1c228998c002
veronica-bernardo <vp.bernardo@icloud.com>
2025-05-29 00:39:00 +0800

    Create REST API and add comments

--- src/main/java/com/kvcode/SpringBootCrudApplication.java
+++ src/main/java/com/kvcode/SpringBootCrudApplication.java
@@ -2,12 +2,24 @@ package com.kvcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+// Bundles @EnableAutoConfiguration, @ComponentScan and @Configuration annotations.
 @SpringBootApplication
+// To start creating REST API.
+@RestController
 public class SpringBootCrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCrudApplication.class, args);
+    }
+
+    // To expose this method as end point.
+    @GetMapping
+    // This method is a REST endpoint.
+    public String helloWorld() {
+        return "Hello World Spring Boot!";
     }
 
 }
