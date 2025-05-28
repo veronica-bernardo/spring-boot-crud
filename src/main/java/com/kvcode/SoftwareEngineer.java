34316d4f490f437c93848cf271e5cd9e13e1818c
veronica-bernardo <vp.bernardo@icloud.com>
2025-05-29 01:23:33 +0800

    Generate equals and hashCode

--- src/main/java/com/kvcode/SoftwareEngineer.java
+++ src/main/java/com/kvcode/SoftwareEngineer.java
@@ -1,5 +1,7 @@
 package com.kvcode;
 
+import java.util.Objects;
+
 public class SoftwareEngineer {
     private Integer id;
     private String name;
@@ -40,6 +42,20 @@ public class SoftwareEngineer {
 
     public void setTechStack(String techStack) {
         this.techStack = techStack;
+    }
+
+    // Equals and Hashcode
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
     }
 }
 
