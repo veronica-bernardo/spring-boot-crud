bf005b9e101807736df1f2c543addb37c930482f
veronica-bernardo <vp.bernardo@icloud.com>
2025-05-29 01:22:19 +0800

    Generate getters and setters

--- src/main/java/com/kvcode/SoftwareEngineer.java
+++ src/main/java/com/kvcode/SoftwareEngineer.java
@@ -15,6 +15,32 @@ public class SoftwareEngineer {
         this.name = name;
         this.techStack = techStack;
     }
+
+    // Getters and setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
 }
 
 
